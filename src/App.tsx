31326fd890896382a30ce066353bf8a2e17bcaa8
altRush <rushing.wp@gmail.com>
2024-08-17 18:03:35 +0700

    feat: Typewritter effect

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import {
+	ChangeEvent,
+	ChangeEventHandler,
+	MutableRefObject,
+	useEffect,
+	useRef,
+	useState
+} from 'react';
+
+const App = () => {
+	type CallbackFunction = (sentence: string) => string | void;
+
+	const [text, setText] = useState('');
+	const [typedSentence, setTypedSentence] = useState('');
+
+	const savedCallback: MutableRefObject<CallbackFunction | undefined> =
+		useRef();
+	const intervalId: MutableRefObject<number | undefined> = useRef();
+
+	// start the interval and save its id,
+	// so we can stop it once all letters are typed
+	const startTyping = (sentence: string) => {
+		setTypedSentence('');
+		intervalId.current = setInterval(() => {
+			if (savedCallback.current) {
+				savedCallback.current(sentence);
+			}
+		}, 500);
+	};
+
+	// stop the interval
+	const stopTyping = () => {
+		clearInterval(intervalId.current);
+		intervalId.current = undefined;
+	};
+
+	const typeNextLetter = (sentence: string) => {
+		if (typedSentence.length === sentence.length) {
+			stopTyping();
+			return;
+		}
+		const nextLetterIndex = typedSentence.length;
+		setTypedSentence(typedSentence + sentence[nextLetterIndex]);
+	};
+
+	useEffect(() => {
+		savedCallback.current = typeNextLetter;
+	}, [typedSentence]);
+
+	const handleChange: ChangeEventHandler<HTMLInputElement> = (
+		e: ChangeEvent<HTMLInputElement | undefined>
+	) => {
+		e.preventDefault();
+
+		setText(e.target.value || '');
+	};
+
+	const typeWrite = () => {
+		startTyping(text);
+	};
+
+	return (
+		<div>
+			<input
+				onChange={handleChange}
+				type="text"
+				name="sentence"
+				placeholder="Type a sentence"
+				style={{ width: '300px', height: '40px' }}
+			/>
+			<button onClick={typeWrite} type="submit">
+				Display with typewriter effect
+			</button>
+
+			{typedSentence && <p>You typed {typedSentence}</p>}
+		</div>
+	);
+};
+
+export default App;
